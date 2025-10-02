3e086870f33593aa6305e33b469fbd4e3a2d52a6
parkkwangkil <parkkwangkil@example.com>
2025-10-02 10:28:33 +0900

    긴급 수정: forms.py에서 Allauth import 제거

--- wbs/forms.py
+++ wbs/forms.py
@@ -1,33 +1,34 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.db import models
-from allauth.account.forms import LoginForm as AllauthLoginForm
+# from allauth.account.forms import LoginForm as AllauthLoginForm  # 비활성화
 from .models import Project, ProjectPhase, Comment, DailyProgress, TaskChecklistItem, UserProfile, SubscriptionPlan, UserSubscription, AdCampaign, Event, PersonalTask
 
-class CustomLoginForm(AllauthLoginForm):
-    """커스텀 로그인 폼 - 이메일 검증 문제 해결"""
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # login 필드를 CharField로 변경하여 더 유연하게 처리
-        self.fields['login'] = forms.CharField(
-            label="이메일 주소",
-            widget=forms.TextInput(attrs={
-                'placeholder': '이메일 주소를 입력하세요',
-                'class': 'form-control login-input'
-            })
-        )
-    
-    def clean_login(self):
-        login = self.cleaned_data.get('login')
-        if not login:
-            raise forms.ValidationError("이메일 주소를 입력해주세요.")
-        
-        # 이메일 형식 간단 검증
-        if '@' not in login or '.' not in login:
-            raise forms.ValidationError("올바른 이메일 형식이 아닙니다.")
-        
-        return login
+# CustomLoginForm 비활성화 (Django 기본 로그인 사용)
+# class CustomLoginForm(AllauthLoginForm):
+#     """커스텀 로그인 폼 - 이메일 검증 문제 해결"""
+#     
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         # login 필드를 CharField로 변경하여 더 유연하게 처리
+#         self.fields['login'] = forms.CharField(
+#             label="이메일 주소",
+#             widget=forms.TextInput(attrs={
+#                 'placeholder': '이메일 주소를 입력하세요',
+#                 'class': 'form-control login-input'
+#             })
+#         )
+#     
+#     def clean_login(self):
+#         login = self.cleaned_data.get('login')
+#         if not login:
+#             raise forms.ValidationError("이메일 주소를 입력해주세요.")
+#         
+#         # 이메일 형식 간단 검증
+#         if '@' not in login or '.' not in login:
+#             raise forms.ValidationError("올바른 이메일 형식이 아닙니다.")
+#         
+#         return login
 
 class ProjectForm(forms.ModelForm):
     class Meta:
