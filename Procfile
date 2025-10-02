655f95ef12334e63a2c73ef11da20b24cd4547e6
parkkwangkil <parkkwangkil@example.com>
2025-10-02 10:18:35 +0900

    긴급 수정: Allauth 미들웨어 제거 및 데이터 로딩 비활성화

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py collectstatic --noinput && python manage.py migrate && python manage.py create_admin && python manage.py loaddata data_backup.json
+release: python manage.py collectstatic --noinput && python manage.py migrate && python manage.py create_admin
 web: gunicorn wbs_project.wsgi:application --bind 0.0.0.0:$PORT
--- wbs_project/settings.py
+++ wbs_project/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
+    # "allauth.account.middleware.AccountMiddleware",  # 비활성화
 ]
 
 ROOT_URLCONF = "wbs_project.urls"
